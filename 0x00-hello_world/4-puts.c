c40b3503b5921fa266227703084a2945b400fcdf
Blen92 <57014283+Blen92@users.noreply.github.com>
2022-04-07 23:30:30 +0300

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,10 @@
-#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Description: prints "\"Programming is like building a multilingual puzzle"
- *
- * Return: 0
- *
- */
-int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");	
-return (0);
-}
+#include <stdio.h> 
+ /** 
+  * Description: main - Prints out a string to stdout. 
+  * Return: 0 if success. 
+  */ 
+ int main(void) 
+ { 
+ puts("\"Programming is like building a multilingual puzzle"); 
+ return (0); 
+ }
